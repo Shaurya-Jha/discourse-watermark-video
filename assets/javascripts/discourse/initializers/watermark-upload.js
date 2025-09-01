16e989cdc246c730131c1e98eb3cf1999223aa22
Shaurya-Jha <shauryajha1602@gmail.com>
2025-09-01 15:51:08 +0530

    fixed linting errors

--- assets/javascripts/discourse/initializers/watermark-upload.js
+++ assets/javascripts/discourse/initializers/watermark-upload.js
@@ -1,48 +1,48 @@
+import bootbox from "bootbox";
 import { ajax } from "discourse/lib/ajax";
 import { withPluginApi } from "discourse/lib/plugin-api";
-import bootbox from "bootbox";
 
 export default {
-    name: "watermark-upload",
+  name: "watermark-upload",
 
-    initialize() {
-        withPluginApi("1.8.0", (api) => {
-            api.addComposerToolbarButton({
-                id: "upload-watermark",
-                group: "extras",
-                icon: "image",
-                title: "Upload Watermark",
-                action: (toolbarEvent) => {
-                    let input = document.createElement("input");
-                    input.type = "file";
-                    input.accept = "image/*";
+  initialize() {
+    withPluginApi("1.8.0", (api) => {
+      api.addComposerToolbarButton({
+        id: "upload-watermark",
+        group: "extras",
+        icon: "image",
+        title: "Upload Watermark",
+        action: (toolbarEvent) => {
+          let input = document.createElement("input");
+          input.type = "file";
+          input.accept = "image/*";
 
-                    input.onchange = () => {
-                        let file = input.files[0];
-                        if (!file) {
-                            return;
-                        }
+          input.onchange = () => {
+            let file = input.files[0];
+            if (!file) {
+              return;
+            }
 
-                        let data = new FormData();
-                        data.append("file", file);
+            let data = new FormData();
+            data.append("file", file);
 
-                        ajax("/watermark/upload", {
-                            type: "POST",
-                            data,
-                            processData: false,
-                            contentType: false,
-                        })
-                            .then((response) => {
-                                toolbarEvent.addText(`![watermark](${response.url})`);
-                            })
-                            .catch(() => {
-                                bootbox.alert("Failed to upload watermark image");
-                            });
-                    };
+            ajax("/watermark/upload", {
+              type: "POST",
+              data,
+              processData: false,
+              contentType: false,
+            })
+              .then((response) => {
+                toolbarEvent.addText(`![watermark](${response.url})`);
+              })
+              .catch(() => {
+                bootbox.alert("Failed to upload watermark image");
+              });
+          };
 
-                    input.click();
-                },
-            });
-        });
-    },
+          input.click();
+        },
+      });
+    });
+  },
 };
